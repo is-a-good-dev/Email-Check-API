5d07faa8f801ee06e4b297af5c04d6cd03eb884b
Will Clarke <clarke19w@gmail.com>
2022-08-13 00:08:47 +0100

    mx records + mx record count

--- index.php
+++ index.php
@@ -27,14 +27,25 @@ if ($_SERVER['REQUEST_METHOD']=='GET'){
     }
     $data = [];
     $data['email'] = $email;
-    $data['domain'] =  substr(strrchr($email, "@"), 1);
+    $domain = substr(strrchr($email, "@"), 1);
+    $data['domain'] =  $domain;
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) $data['domain_data']['valid_format'] = true;
     else $data['domain_data']['valid_format'] = false;
     if ($data['domain_data']['valid_format']) {
         if (DisposableEmail::isDisposable($email)) $data['domain_data']['disposable_email'] = true;
         $data['domain_data']['disposable_email'] = false;
     }
-    
+    $dns_data = dns_get_record($domain,DNS_MX);
+    //echo $domain;
+    $arr_len = count($dns_data);
+    $data['domain_data']['mx_record_count'] = $arr_len;
+    $i = 0;
+    $data['domain_data']['mx_records'] = array();
+    while ($i < $arr_len) {
+        array_push($data['domain_data']['mx_records'], $dns_data[$i]['target']);
+        $i++;
+    }
+    //var_dump($data['domain_data']['mx_records']);
 
     
     response(200, 'OK', $data);
