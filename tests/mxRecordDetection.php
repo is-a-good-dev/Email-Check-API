60ae98c3519dbd0fa708ab9b831f23e9c24b8a8c
Will Clarke <clarke19w@gmail.com>
2022-08-12 23:53:56 +0100

    test: put all mx records in array and compare to actual domain

--- tests/mxRecordDetection.php
+++ tests/mxRecordDetection.php
@@ -1,2 +1,25 @@
 <?php
-var_dump( dns_get_record('google.com',DNS_MX));
+var_dump( dns_get_record('wclarke.dev',DNS_MX));
+
+$mx_record = [];
+$mx_record['record'] = $dns[0]['target'];
+$mx_record['ttl'] = $dns[0]['ttl'];
+$mx_record['pri'] = $dns[0]['pri'];
+//echo "<br>" . $mx_record['ttl'];
+//var_dump(dns_get_record('google.com', DNS_NS));
+//echo "<br>". checkdnsrr('wclarke.dev','MX'); // How many mx records 
+echo "<br>";
+
+
+// Put all mx records into array
+$dns = dns_get_record('wclarke.dev',DNS_MX);
+$count = count($dns);
+echo $count;
+$i = 0;
+$record = array();
+while ($i < $count) {
+    array_push($record, $dns[$i]['target']);
+    $i++;
+}
+echo "<br>";
+var_dump($record);
