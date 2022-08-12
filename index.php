e115ac1bc23ac6a3f9a15fdd24f585f311a744dc
Will Clarke <clarke19w@gmail.com>
2022-08-12 08:55:48 +0100

    change the function to remove stupid shit

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php 
 header('Content-type: application/json');
-function json_response($type, $message, $status ,$data=null){
+function json_response($status,$message,&$data=null){
     header("HTTP/1.1 ".$status);
     $response['status'] = $status;
-    $response['message'] = $type."~ ".$message;
+    $response['message'] = $message;
     if ($data != null) $response['data'] = $data;
     echo json_encode($response);
 }
 error_reporting(0); 
 if ($_SERVER['REQUEST_METHOD']=='GET'){
     $key = $_GET['key'];
-    if (empty(trim($key))) json_response('error', 'API key cannot be empty', 400);
+    if (empty(trim($key))) json_response(400, 'API key cannot be empty');
+
 }
