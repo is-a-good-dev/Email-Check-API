f7c51651bbe6e3b12f47dfb7601681543c160426
Will Clarke <clarke19w@gmail.com>
2022-10-10 21:29:21 +0100

    ill figure it out tmr

--- api.php
+++ api.php
@@ -1,18 +1,33 @@
 <?php
-class response {
+class api {
+    public function __construct () {
+        header ('Content-type: application/json');
+    }
+}
+class response extends api {
     public string $message;
     public int $status;
     public mixed $data;
+    public mixed $response;
 
-    public function __construct ($message, $status, $data) {
-        $this->message = $message; 
-        $this->status = $status;
-        $this->data = $data;
+    public function __construct (string $message, int $status, mixed $data = null, mixed $response = null) {
 
+        response::responde($message, $status, $data);
+    }
+
+    public static function responde ($message, $status, $data = null) {
+        header ("HTTP/1.1 ". $this->status);
+        
+        $this->r = array();
+        $this->r['status'] = $this->status;
+        $this->r['message'] = $this->message;
+        if (isset($this->data)) $this->r['data'] = $this->data;
+
+        echo json_encode($this->r);
     }
 }
 
-class data {
+class validate extends api {
     public mixed $return_data;
 
     public function __construct (mixed $return_data = null) {
@@ -20,9 +35,21 @@ class data {
         $this->return = $return_data;
 
     }
+
+    public function rm_email (string $email) {
+        return substr(strrchr($email, "@"), 1);
+    }
+
+    public function email (string $email) {
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) $this->return = true;
+        $this->return = false;
+
+    }
+
     public function mx (string $validate) {
 
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) $validate = substr(strrchr($email, "@"), 1);
+        if (filter_var($validate, FILTER_VALIDATE_EMAIL)) $validate = substr(strrchr($email, "@"), 1);
 
         $dns = dns_get_record ($validate, DNS_MX);
         
@@ -33,9 +60,10 @@ class data {
 
         $this->return = $mx_records;
     }
+
     public function __destruct() {
 
         if (isset($this->return)) return $this->return;
-        
+
     }
 }
--- main.php
+++ main.php
@@ -1,5 +1,11 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-$router = new  \Bramus\Router\Router();
 use ElliotJReed\DisposableEmail\DisposableEmail;
-header('Content-type: application/json');
+require 'api.php';
+new api();
+error_reporting(0);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $email = $_GET['email'];
+
+    $validate = new validate;
+    if (!$validate->email($email)) $r = new response(204, 'Email format not supported.');
+}
